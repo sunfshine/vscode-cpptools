87570fb091d157f671aee86e424d6b0b81fb632e
Andrew Wang <waan@waan-macbook-pro.local>
2019-02-12 15:59:43 -0800

    Increase timeout time

--- Extension/test/integrationTests/index.ts
+++ Extension/test/integrationTests/index.ts
@@ -28,7 +28,7 @@ var testRunner = require('vscode/lib/testrunner');
 testRunner.configure({
 	ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
 	useColors: true, // colored output from test results
-    timeout: 60000
+    timeout: 600000
 });
 
 const logFolder = path.join( __dirname, ".logs")
